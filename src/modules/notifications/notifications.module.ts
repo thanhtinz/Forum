1aca6933c50397bc18b88a70e8a504a4d28ce280
thanhtinz <thanhtinz23072003@gmail.com>
2026-06-16 14:57:27 +0000

    Thông báo realtime: NotificationsGateway (/notif) push live + badge chưa đọc trên chuông
    - Gateway namespace /notif: auth JWT, join user:{id}, @OnEvent('notification.created') -> emit tới user
    - Header bell: fetch unreadCount + socket /notif tăng badge realtime; click reset
    - next build 48 trang OK; boot DI sạch

--- src/modules/notifications/notifications.module.ts
+++ src/modules/notifications/notifications.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { NotificationsService } from './notifications.service';
 import { NotificationsController } from './notifications.controller';
+import { NotificationsGateway } from './notifications.gateway';
 
 @Module({
-  providers: [NotificationsService],
+  imports: [JwtModule.register({})],
+  providers: [NotificationsService, NotificationsGateway],
   controllers: [NotificationsController],
   exports: [NotificationsService],
 })
